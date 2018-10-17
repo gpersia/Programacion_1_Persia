982f5f012476b115c7ba0b41257f4559d5573bbe
Gianluca <you@example.com>
2018-10-17 16:49:37 -0300

    TP4

---
+++ TP4/Ejercicio 2/ejercicio2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<html lang="en" dir="ltr">
+  		<head>
+    		<meta charset="utf-8">
+    		<title>EJERCICIO 2</title>
+  		</head>
+  	<body>
+    	<table border="1">
+		<?php
+    		$server="localhost";
+    		$user="root";
+    		$pass="ruloqueado";
+    		$db="programacion1";
+    		$connect=new PDO("mysql: host=$server;dbname=$db",$user,$pass);
+    		$sql="select * from persona";
+    		$runsql=$connect->prepare($sql);
+    		$runsql->execute();
+    			while($fila=$runsql->fetch(PDO::FETCH_ASSOC)){
+      				echo "<tr>";
+      				foreach($fila as $filaa){
+        			echo "<td>";
+        			echo $filaa;
+        			echo "</td>";
+      				}
+      				echo "</tr>";
+   				}
+    	?>
+		</table>
+	</body>
+</html>
